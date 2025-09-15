1e13e60ec3a76cf76029acd5dbb95738894ebf85
userRyad <ryadkhenno09@gmail.com>
2025-09-14 22:24:40 -0400

    first update

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ from langchain_anthropic import ChatAnthropic
 
 load_dotenv() #to load  env variables files (.env)
 
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+response=llm.invoke("Whhy dont't we ride cows?")
+print(response)
